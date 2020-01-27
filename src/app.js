844af381cf5b12a86aa721b5998ca2195efe2bcd
Martin Leitner <martin_leitner@live.at>
2020-01-27 21:07:52 +0100

    First commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+window.$ = window.jQuery = require('jquery');
+
+function colorMe(i){
+    let square = $('div.container div:eq(' + i + ')');
+    setTimeout(function(){
+        i % 2 ? square.addClass('white') : square.addClass('black');
+    }, i * 1000)
+}
+
+for(let i = 0; i < 9; i++) {
+    colorMe(i);
+}
